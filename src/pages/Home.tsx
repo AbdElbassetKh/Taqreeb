034750791d70e489dcaa17d5657338f0bf437f89
AbdElbassetKh <abdelbassetkhettabi@gmail.com>
2024-11-12 23:16:41 +0100

    Reset

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import FeaturedBooks from '../components/FeaturedBooks';
+import Categories from '../components/Categories';
 
 const Home = () => {
   return (
